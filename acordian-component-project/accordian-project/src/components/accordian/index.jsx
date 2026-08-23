7840cc915267e3f4538decc5b2608eacceb66f32
AkifNaveed12 <akif27031@gmail.com>
2026-08-23 13:32:08 +0500

    Accordian single component project

---
+++ acordian-component-project/accordian-project/src/components/accordian/index.jsx
@@ -0,0 +1,45 @@
+// interview question: create an accordian component 
+
+import {React, useState} from 'react';
+import data from './data'
+import './styles.css'
+
+//single selection  -> simple variation
+//multi selection -> difficult variation
+const Accordian = () => {
+
+    const [selected, setSelected] = useState(null)
+
+    //single selection function
+    function handleSingleSelection(getCurrentId) {
+        console.log(getCurrentId)
+        setSelected(getCurrentId === selected ? null : getCurrentId);    }
+    console.log(selected)
+    return (
+        <div className='wrapper'>
+            <div className='accordian'>
+                {
+                    data && data.length > 0 ?  data.map(dataItem => <div className='item' key={dataItem.id}>
+                        <div className='title' 
+                            onClick={() => handleSingleSelection(dataItem.id)}>
+                            
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {/* single selection implementation */}
+                        {
+                            selected === dataItem.id ? 
+                            <div className='content'>
+                                {dataItem.answer} 
+                            </div>
+                            : null
+                        }
+
+                    </div> ) : ( <div> No item Found !</div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Accordian;
